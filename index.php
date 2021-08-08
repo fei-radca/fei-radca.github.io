7715aef371901569b2143be4c522815cf84da7f1
Simona735 <radca.fei@speai.sk>
2021-08-08 18:02:01 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="sk">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="FEI radca">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" type="text/css" href="css/default.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>FEI radca</title>
+</head>
+
+<body class="bg-light">
+</body>
+</html>
+
